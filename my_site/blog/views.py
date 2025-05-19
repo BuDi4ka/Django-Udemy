2e325c637c90761f18385726de1fab2a3c93c85e
Dmytro Burdeniuk <dmytroburdenuk@gmail.com>
2025-05-19 17:14:41 +0100

    own practice

--- my_site/blog/views.py
+++ my_site/blog/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+def index(request):
+    return HttpResponse("Welcome page")
+
+
+def posts(request):
+    return HttpResponse("All posts")
+
+
+def post(request, post_id):
+    return HttpResponse(f"Post {post_id}")
