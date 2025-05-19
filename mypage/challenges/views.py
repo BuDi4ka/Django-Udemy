e1e988f91f65e7ebd96076ab4d1c998b78345761
Dmytro Burdeniuk <dmytroburdenuk@gmail.com>
2025-05-19 11:28:24 +0100

    reverse functions

--- mypage/challenges/views.py
+++ mypage/challenges/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse, HttpResponseNotFound, HttpResponseRedirect
+from django.urls import reverse 
 
 
 monthly_challenges = {
@@ -23,7 +24,8 @@ def monthly_challenge_by_number(request, month):
     if month > len(months):
         return HttpResponseNotFound('Invalid month')
     
-    redirect_month = months[month]
+    redirect_month = months[month - 1]
+    redirect_path = reverse('month-challenge', args=[redirect_month])
     return HttpResponseRedirect('/challenges/' + redirect_month)
 
 
