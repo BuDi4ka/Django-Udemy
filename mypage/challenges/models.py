e8f9d4a94be2da502993e04b90f26375a6888179
Dmytro Burdeniuk <dmytroburdenuk@gmail.com>
2025-05-20 15:45:28 +0100

    first models

--- mypage/challenges/models.py
+++ mypage/challenges/models.py
@@ -1,5 +1,9 @@
 from django.db import models
+from django.db.models import AutoField
+
 
 # Create your models here.
 class Book(models.Model):
-    title = models.CharField(max_length=100)
+    id = AutoField(primary_key=True, unique=True)
+    title = models.CharField(max_length=50)
+    rating = models.Field()
