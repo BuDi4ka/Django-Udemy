b324aa20b775ccfdb997e0f00deea962714fc52f
Dmytro Burdeniuk <dmytroburdenuk@gmail.com>
2025-05-20 18:34:45 +0100

    rendering details page

--- data_models/dat_model/urls.py
+++ data_models/dat_model/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index)
+    path('', views.index),
+    path('<int:book_id>', views.detail),
 ]
--- data_models/dat_model/views.py
+++ data_models/dat_model/views.py
@@ -1,4 +1,5 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from django.http import Http404
 
 from .models import Book
 
@@ -11,3 +12,21 @@ def index(request):
                   'dat_model/index.html',
                   {'books': books}
                   )
+
+
+def detail(request, book_id):
+    # try:
+    #     book = Book.objects.get(pk=book_id)
+    # except:
+    #     raise Http404
+    book = get_object_or_404(Book, pk=book_id)
+    return render(
+        request,
+        'dat_model/book_detail.html',
+        {
+            'title': book.title,
+            'author': book.author,
+            'rating': book.rating,
+            'is_bestseller': book.is_bestselling,
+        }
+    )
