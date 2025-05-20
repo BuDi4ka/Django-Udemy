7333c928c7dd11270639bedc468270c3c721087f
Dmytro Burdeniuk <dmytroburdenuk@gmail.com>
2025-05-20 12:34:21 +0100

    dummy data to views

--- my_site/blog/views.py
+++ my_site/blog/views.py
@@ -1,6 +1,26 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+from datetime import date
+
+posts = [
+    {
+        'slug': 'hike-in-the-mountains',
+        'image': 'mountains.jpg',
+        'author': 'Dima',
+        'date': date(2022, 9, 12),
+        'tittle': 'Mountain Hiking',
+        'excerpt': "There's nothing like the views you get when hiking in the mountains.",
+        'content': '''
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquam labore laudantium quidem quisquam
+            sapiente?
+            A adipisci amet assumenda aut eveniet iste iure nemo numquam quo temporibus velit veniam, vero
+            voluptatibus?
+        '''
+    }
+]
+
+
 # Create your views here.
 def index(request):
     return render(request, 'blog/index.html')
