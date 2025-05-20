559187d9ea52d1748b906617d0f1c9485f4ad5e8
Dmytro Burdeniuk <dmytroburdenuk@gmail.com>
2025-05-20 16:35:25 +0100

    inserting data

--- mypage/challenges/models.py
+++ mypage/challenges/models.py
@@ -1,9 +1,7 @@
 from django.db import models
-from django.db.models import AutoField
 
 
 # Create your models here.
 class Book(models.Model):
-    id = AutoField(primary_key=True, unique=True)
     title = models.CharField(max_length=50)
-    rating = models.Field()
+    rating = models.IntegerField()
