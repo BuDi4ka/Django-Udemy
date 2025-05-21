6d50fc8229f8e331c5661d532e7a97f5f64497e2
Dmytro Burdeniuk <dmytroburdenuk@gmail.com>
2025-05-21 12:19:39 +0100

    more conf admin

--- data_models/dat_model/admin.py
+++ data_models/dat_model/admin.py
@@ -5,6 +5,8 @@ from .models import Book
 # Register your models here.
 class BookAdmin(admin.ModelAdmin):
     prepopulated_fields = {'slug': ('title',)}
+    list_filter = ('author','rating')
+    list_display = ('title','author','rating')
 
 
 admin.site.register(Book, BookAdmin)
