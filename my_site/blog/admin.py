623c8b0118d9050e37f2e3ed52b97667bbb8b1c3
Dmytro Burdeniuk <dmytroburdenuk@gmail.com>
2025-05-21 17:28:30 +0100

    admin settings

--- my_site/blog/admin.py
+++ my_site/blog/admin.py
@@ -2,7 +2,12 @@ from django.contrib import admin
 
 from .models import Post, Author, Tag
 
+class PostAdmin(admin.ModelAdmin):
+    list_filter = ('date', 'author', 'tags')
+    list_display = ('title', 'date', 'author')
+    prepopulated_fields = {'slug': ('title',)}
+
 # Register your models here.
-admin.site.register(Post)
+admin.site.register(Post, PostAdmin)
 admin.site.register(Author)
 admin.site.register(Tag)
