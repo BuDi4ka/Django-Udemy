60c89d634fdecb9a2efd1eacd7ddd669fac3ab85
Dmytro Burdeniuk <dmytroburdenuk@gmail.com>
2025-05-22 11:14:42 +0100

    modelform intro

--- feedback/reviews/forms.py
+++ feedback/reviews/forms.py
@@ -1,22 +1,26 @@
 from django import forms
 from .models import Review
 
-class ReviewForm(forms.Form):
-    user_name = forms.CharField(
-        label="Your name",
-        max_length=100,
-        error_messages={
-            "required": "Please enter your name",
-            "max_length": "Name is too long (max 100 characters)",
-        },
-    )
-    review_text = forms.CharField(
-        label="Your feedback",
-        widget=forms.Textarea,
-        max_length=200,
-    )
-    rating = forms.IntegerField(label="Your rating", min_value=1, max_value=5)
 
+# class ReviewForm(forms.Form):
+#     user_name = forms.CharField(
+#         label="Your name",
+#         max_length=100,
+#         error_messages={
+#             "required": "Please enter your name",
+#             "max_length": "Name is too long (max 100 characters)",
+#         },
+#     )
+#     review_text = forms.CharField(
+#         label="Your feedback",
+#         widget=forms.Textarea,
+#         max_length=200,
+#     )
+#     rating = forms.IntegerField(label="Your rating", min_value=1, max_value=5)
+#
+
+
+class ReviewForm(forms.ModelForm):
     class Meta:
         model = Review
-        fields = ["user_name", "review_text", "rating"]
+        fields = '__all__'
