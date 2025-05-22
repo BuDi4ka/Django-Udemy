1288e564e068fffc63eb4faa674dc1bd238126f8
Dmytro Burdeniuk <dmytroburdenuk@gmail.com>
2025-05-22 09:57:28 +0100

    form class

--- feedback/reviews/views.py
+++ feedback/reviews/views.py
@@ -1,21 +1,21 @@
 from django.shortcuts import render, redirect
-
+from .forms import ReviewForm
 
 # Create your views here.
 
 def review(request):
     if request.method == "POST":
-        entered_username = request.POST['username']
-        if entered_username == '':
-            return render(request, 'reviews/review.html',
-                          {'has_error': True,
-                           })
-        print(entered_username)
-        return redirect('thank-you')
+        form = ReviewForm(request.POST)
+        if form.is_valid():
+            print(form.cleaned_data)
+            return redirect('thank-you')
+
+    form = ReviewForm()
 
     return render(request, 'reviews/review.html', {
-        'has_error': False,
+        'form': form,
     })
+
 
 def thank_you(request):
     return render(request, 'reviews/thank_you.html')
