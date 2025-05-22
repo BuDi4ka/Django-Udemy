1038a154861df8bc56055bc611b2a9a1e3234cb1
Dmytro Burdeniuk <dmytroburdenuk@gmail.com>
2025-05-22 10:27:13 +0100

    customizing form controls

--- feedback/reviews/forms.py
+++ feedback/reviews/forms.py
@@ -1,6 +1,12 @@
 from django import forms
 
+
 class ReviewForm(forms.Form):
-    user_name = forms.CharField()
-
-
+    user_name = forms.CharField(
+        label="Your name",
+        max_length=100,
+        error_messages={
+            "required": "Please enter your name",
+            "max_length": "Name is too long (max 100 characters)",
+        },
+    )
