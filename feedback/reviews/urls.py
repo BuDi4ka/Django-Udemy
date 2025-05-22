d3076881030cf987dd33106a4d61e503565f46e3
Dmytro Burdeniuk <dmytroburdenuk@gmail.com>
2025-05-22 13:41:11 +0100

    FormView

--- feedback/reviews/urls.py
+++ feedback/reviews/urls.py
@@ -4,7 +4,6 @@ from . import views
 urlpatterns = [
     path('', views.ReviewView.as_view(), name='review'),
     path('thank_you/', views.ThankYouView.as_view(), name='thank-you'),
-    # path('all-reviews/', views.ReviewListView.as_view(), name='all-reviews'),
-    path('all-reviews/', views.ReviewsListView.as_view(), name='all-reviews'),
+    path('all-reviews/', views.ReviewListView.as_view(), name='all-reviews'),
     path('review/<int:pk>/', views.ReviewDetail.as_view(), name='review-detail'),
 ]
--- feedback/reviews/views.py
+++ feedback/reviews/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, redirect
 from django.views import View
 from django.views.generic import ListView, DetailView
 from django.views.generic.base import TemplateView
+from django.views.generic.edit import FormView
 
 from .forms import ReviewForm
 from .models import Review
@@ -10,19 +11,14 @@ from .models import Review
 # Create your views here.
 
 
-class ReviewView(View):
-    def get(self, request):
-        form = ReviewForm()
-        return render(request, "reviews/review.html", {"form": form})
+class ReviewView(FormView):
+    form_class = ReviewForm
+    template_name = 'reviews/review.html'
+    success_url = '/thank_you/'
 
-    def post(self, request):
-        form = ReviewForm(request.POST)
-
-        if form.is_valid():
-            form.save()
-            return redirect("thank-you")
-
-        return render(request, "reviews/review.html", {"form": form})
+    def form_valid(self, form):
+        form.save()
+        return super().form_valid(form)
 
 
 class ThankYouView(TemplateView):
@@ -34,22 +30,13 @@ class ThankYouView(TemplateView):
         return context
 
 
-class ReviewsListView(TemplateView):
-    template_name = 'reviews/all_reviews.html'
-
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-        context['reviews'] = Review.objects.all()
-        return context
-
-
 class ReviewDetail(DetailView):
     model = Review
     template_name = 'reviews/review_detail.html'
     context_object_name = 'review'
 
 
-# class ReviewListView(ListView):
-#     model = Review
-#     template_name = "reviews/all_reviews.html"
-#     context_object_name = "reviews"
+class ReviewListView(ListView):
+    model = Review
+    template_name = "reviews/all_reviews.html"
+    context_object_name = "reviews"
