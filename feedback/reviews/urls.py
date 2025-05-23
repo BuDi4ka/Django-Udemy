939cb225f71de22047277eb6a254509bbaf580e3
Dmytro Burdeniuk <dmytroburdenuk@gmail.com>
2025-05-23 10:30:52 +0100

    fixing sessions

--- feedback/reviews/urls.py
+++ feedback/reviews/urls.py
@@ -5,7 +5,7 @@ from . import views
 urlpatterns = [
     path("", views.ReviewView.as_view()),
     path("thank-you", views.ThankYouView.as_view()),
-    path("reviews", views.ReviewsListView.as_view()),
+    path("reviews", views.ReviewsListView.as_view(), name='reviews-list'),
     path("favorite", views.AddFavoriteView.as_view(), name='favorite-review'),
     path("reviews/<int:pk>", views.SingleReviewView.as_view(), name="single-review"),
 ]
